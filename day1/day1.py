a0099c6c8ffa00c019da97f52277d8905bc8216d
Yasmin2k3 <119609274+Yasmin2k3@users.noreply.github.com>
2022-12-01 15:12:57 +0000

    Update day1.py

--- day1/day1.py
+++ day1/day1.py
@@ -4,6 +4,7 @@ f.close()
 
 rawStrings = sr.split("\n")
 
+#part 1:
 calorieList = []
 calorieElf = []
 
@@ -23,5 +24,6 @@ for x in range (len(calorieList)):
 calorieList.sort()
 print("Value of elf with most amount of calories:", calorieList[-1])
 
+#part 2:
 topThreeValue = calorieList[-1] + calorieList[-2] + calorieList[-3]
 print("Calories carried by the top three elves in total:", topThreeValue)
