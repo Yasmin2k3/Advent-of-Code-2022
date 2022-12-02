e3a435bd1b738ad4a908f33dd0b3396e2ffdb5d3
Yasmin2k3 <119609274+Yasmin2k3@users.noreply.github.com>
2022-12-02 11:41:43 +0000

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,23 @@
+f = open("day1Text.txt", "r")
+sr = f.read()
+f.close()
+
+rawStrings = sr.split("\n")
+
+calorieList = []
+calorieElf = []
+
+for a in rawStrings:
+    if a == '':
+        calorieList.append(calorieElf)
+        calorieElf = []
+    else:
+        calorieElf.append(int(a))
+    
+result = list(map(sum, calorieList))
+result.sort()
+
+print("Value of elf with most amount of calories:", result[-1])
+
+topThreeValue = result[-1] + result[-2] + result[-3]
+print("Calories carried by the top three elves in total:", topThreeValue)
